260737f8d593b397c2fbd0f8a01c453c8aa272c2
band343 <45181573+band343@users.noreply.github.com>
2018-11-19 12:52:12 -0800

    Add files via upload

---
+++ Follow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+    public GameObject toFollow;
+    private Vector3 offset;
+    public bool follow_x = true;
+    public bool follow_y = true;
+    public bool follow_z = true;
+
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - toFollow.transform.position;
+	}
+	
+	// LateUpdate is called once per frame after Update has been run
+	void Update () {
+        Vector3 diff = toFollow.transform.position + offset;
+        float x = follow_x ? diff.x : transform.position.x;
+        float y = follow_y ? diff.y : transform.position.y;
+        float z = follow_z ? diff.z : transform.position.z;
+        transform.position = new Vector3(x, y, z);
+    }
+}
